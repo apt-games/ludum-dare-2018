113d2cff3b384b36ff43c937f8ae81b524730da3
Anders Havskjold <anders.havskjold@apt.no>
2018-12-01 12:55:18 +0100

    Added crude player controller and map controller

---
+++ ludum-dare-43/Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public MapController MapController;
+
+    public List<CharacterBehaviour> Players;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    MapController.RoomSelected += OnRoomSelected;
+	}
+
+    private void OnRoomSelected(RoomBehaviour obj)
+    {
+        foreach (var player in Players)
+        {
+            player.MoveTo(obj.transform);
+        }
+    }
+}
---
+++ ludum-dare-43/Assets/Scripts/Room/RoomBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+public class RoomBehaviour : MonoBehaviour
+{
+    public event Action<RoomBehaviour> Selected;
+
+    public void OnMouseOver()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("clicked "+name);
+            Selected?.Invoke(this);
+        }
+    }
+}
