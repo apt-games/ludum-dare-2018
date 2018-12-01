71776369f6884027b04156184f657436dc624fcb
Daniel Skogly <daniel@apt.no>
2018-12-01 22:52:44 +0100

    Very minor UI progress

---
+++ Assets/Scripts/CharacterAvatar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Animator))]
+public class CharacterAvatar : MonoBehaviour {
+
+    private Animator _animator;
+    public GameObject actions;
+
+    public void OnMouseEnter () {
+        Debug.Log("Enter!");
+
+        _animator.SetTrigger("showactions");
+    }
+
+    public void OnMouseExit () {
+        Debug.Log("Exit!");
+
+        _animator.SetTrigger("hideactions");
+    }
+
+    private void Awake() {
+        _animator = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
