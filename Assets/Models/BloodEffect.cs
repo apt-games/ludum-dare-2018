e8eac3d21aba0f9af7631a3b9ef7ec1d1c5b733c
Anders Havskjold <anders.havskjold@apt.no>
2018-12-01 21:56:31 +0100

    Added character and animations

---
+++ Assets/Models/BloodEffect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodEffect : MonoBehaviour {
+
+
+public GameObject bloodParticles;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+void Die () {
+Instantiate(bloodParticles, new Vector3(transform.position.x, transform.position.y, transform.position.z - 0.2f), transform.rotation);
+}
+
+}
--- Assets/Scripts/Character/CharacterBehaviour.cs
+++ Assets/Scripts/Character/CharacterBehaviour.cs
@@ -6,9 +6,14 @@ public class CharacterBehaviour : MonoBehaviour
 {
     private NavMeshAgent _agent;
 
+    public Animator Animator;
+
+    private bool _isWalking;
+
     private void Awake ()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = true;
     }
 
     public void MoveTo(Transform room)
@@ -18,5 +23,20 @@ public class CharacterBehaviour : MonoBehaviour
         var target = new Vector3(room.position.x, room.position.y, transform.position.z);
 
         _agent.SetDestination(target);
+        SetWalking(true);
+    }
+
+    private void SetWalking(bool walking)
+    {
+        _isWalking = walking;
+        Animator.SetBool("walking", _isWalking);
+    }
+
+    private void Update()
+    {
+        if (_isWalking && !_agent.hasPath)
+        {
+            SetWalking(false);
+        }
     }
 }
