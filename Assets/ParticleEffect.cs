c7dc4d0a0e266200227fbcde35ac87af2bc51fcc
Anders Havskjold <Anders@Havskjold.no>
2018-12-03 22:36:20 +0100

    POTENTIALFIX: Triggering particle effects with script

---
+++ Assets/ParticleEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleEffect : MonoBehaviour
+{
+    private ParticleSystem _particles;
+
+    void Awake()
+    {
+        _particles = GetComponent<ParticleSystem>();
+    }
+
+    public void PlayParticleEffect()
+    {
+        _particles?.Play();
+    }
+}
